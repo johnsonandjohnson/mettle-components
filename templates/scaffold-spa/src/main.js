08170d66b0835b8035656286aa48de9ac4d769a0
Gabe <gforti@its.jnj.com>
2022-02-06 11:10:11 -0500

    docs: step 1 complete

---
+++ templates/scaffold-spa/src/main.js
@@ -0,0 +1,9 @@
+function component() {
+  const element = document.createElement('div')
+
+  element.innerHTML = `<h1>Hello Mettle</h1>`
+
+  return element
+}
+
+document.body.appendChild(component())
---
+++ templates/scaffold-spa/webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { resolve } = require('path')
+
+module.exports = (env, argv) => {
+  const IS_BUILD = env.WEBPACK_BUILD
+  return {
+    devServer: {
+      historyApiFallback: {
+        rewrites: [
+          { from: /.*\.html?/, to: '/' },
+          { from: /^[\w/]+$/, to: '/' },
+        ],
+        verbose: true
+      },
+      host: '0.0.0.0',
+      open: true,
+      port: 2022,
+    },
+    entry: './src/main.js',
+    module: {
+      rules: [
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.html$/i,
+          exclude: /index\.html$/i,
+          type: 'asset/source',
+        },
+      ],
+    },
+    output: {
+      assetModuleFilename: 'assets/[name][ext]',
+      clean: true,
+      filename: 'main.js',
+      path: resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        base: IS_BUILD ? false : '/',
+        inject: 'body',
+        template: './src/index.html',
+        scriptLoading: 'module'
+      }),
+    ],
+  }
+}
