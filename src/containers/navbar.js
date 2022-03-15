785793e740390efd62b1caa5902897a0eed6b365
aanikiej <aanikiej@its.jnj.com>
2022-03-15 14:50:28 -0400

    added more attribute functionality, left-sided and right-sided navbar, custom close icon, custom navbar width

--- src/containers/navbar.js
+++ src/containers/navbar.js
@@ -10,15 +10,13 @@ if (!window.customElements.get(TAG_NAME)) {
       this._allowedPositions = new Set(['left', 'right'])
       this.$navbarNav = this.shadowRoot.querySelector('.navbar-nav')
       this.$navbar = this.shadowRoot.querySelector('.navbar-open')
-
+      this.isShowing = false
+      this.position = 'left'
+      this.width = this.hasAttribute('nav-width') ? this.getAttribute('nav-width') : '10rem'
+      this.$navbarNav.style.width = this.width
       //see if there is a specified icon or if default one should be used
-      if (!this.hasAttribute('icon')) {
-        this.shadowRoot.querySelector('.navbar-icon').src = 'https://freesvg.org/img/menu-icon.png'
-        this.shadowRoot.querySelector('.navbar-icon-inside').src = 'https://freesvg.org/img/menu-icon.png'
-      } else {
-        this.shadowRoot.querySelector('.navbar-icon').src = this.getAttribute('icon')
-        this.shadowRoot.querySelector('.navbar-icon-inside').src = this.getAttribute('icon')
-      }
+      this.shadowRoot.querySelector('.navbar-icon').src = this.hasAttribute('icon') ? this.getAttribute('icon') : 'https://freesvg.org/img/menu-icon.png'
+      this.shadowRoot.querySelector('.navbar-icon-inside').src = this.hasAttribute('close-icon') ? this.getAttribute('close-icon') : 'https://freesvg.org/img/menu-icon.png'
     }
 
     _generateTemplate() {
@@ -32,23 +30,10 @@ if (!window.customElements.get(TAG_NAME)) {
         .navbar-nav {
           position: fixed;
           top: 0;
-          left: 0;
           height: 100%;
           background-color: lightgray;
           overflow-x: hidden;
           transition: transform 150ms ease-in-out 25ms;
-          width: 10rem;
-          transform: translate(-10rem)
-        }
-        ul {
-          list-style-type: none;
-          padding-inline-start: 0px;
-        }
-        li a {
-          display: block;
-          padding: 20px 30px;
-          font-size: 25px;
-          text-decoration: none;
         }
         .navbar-open {
           cursor: pointer;
@@ -59,57 +44,88 @@ if (!window.customElements.get(TAG_NAME)) {
           padding-left: .5rem;
           padding-top: .5rem;
         }
+        .nav-open {
+          overflow: hidden;
+          height: 100%;
+        }
         </style>
         <div class="navbar-container">
           <div class="navbar-open">
             <img class="navbar-icon">
           </div>
-          <nav class="navbar-nav">
+          <nav class="navbar-nav" part="nav">
             <div class="navbar-nav-close">
               <img class="navbar-icon-inside">
             </div>
-            <ul>
-              <li><a href="#">Home</a></li>
-              <hr></hr>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact Us</a></li>
-              <li><a href="#">Profile</a></li>
-            </ul>
+            <slot name="navbar-content"> </slot>
           </nav>
         </div>
       `
       return template
     }
 
+    //make it so menu can have a set width DONE
+      //transition needs to take this in account when doing translate DONE
+      //don't have width hard coded in DONE
+    //option to choose if menu blocks scrolling 
+    //make it so it doesn't necessarily take an image for the nav icon and have the inside icon have the option of 
+      //being a close button instead of the nav icon again
+
+
     connectedCallback() {
       this._positionAt()
-      //openMenu
+      this._hide()
+      //closeMenu
       this.shadowRoot.querySelector('.navbar-open').addEventListener('click', () => {
         this._toggleNav()
-        this.$navbarNav.style.transform = 'translate(0rem)'
-        this.$navbar.setAttribute('hidden', 'hidden')
+        //this.$navbarNav.style.transform = 'translate(0rem)' //make class that adds and removes
+        //this.$navbar.setAttribute('hidden', 'hidden')
       })
-      //closeMenu
+      //openMenu
       this.shadowRoot.querySelector('.navbar-nav-close').addEventListener('click', () => {
-        //this.$navbarNav.style.width = '0rem'
-        this.$navbarNav.style.transform = 'translate(-10rem)'
-        this.$navbar.removeAttribute('hidden')
+        this._toggleNav()
+        //this.$navbarNav.style.transform = `translate(-${this.width})`
+        //this.$navbar.removeAttribute('hidden')
       })
     }
 
     _toggleNav() {
-      if (this.isShowing()) {
+      if (this.isShowing) {
         this._hide()
       } else {
         this._show()
       }
     }
 
+    _show() {
+      this.$navbarNav.style.transform = 'translate(0rem)'
+      //this.$navbar.removeAttribute('hidden')
+
+
+      
+      this.isShowing = true
+    }
+
+    _hide() {
+      if (this.position === 'left') {
+        this.$navbarNav.style.transform = `translate(-${this.width})`
+      } else {
+        this.$navbarNav.style.transform = `translate(${this.width})`
+      }
+
+      //this.$navbar.setAttribute('hidden', 'hidden')
+      this.isShowing = false
+    }
+
     _positionAt() {
       const attrPosition = this.getAttribute('data-position')
-      const position = this._allowedPositions.has(attrPosition) ? attrPosition : 'left'
+      this.position = this._allowedPositions.has(attrPosition) ? attrPosition : 'left'
 
-      this.$navbar
+      if (this.position === 'left') {
+        this.$navbarNav.style.left = '0'
+      } else {
+        this.$navbarNav.style.right = '0'
+      }
 
     }
 
