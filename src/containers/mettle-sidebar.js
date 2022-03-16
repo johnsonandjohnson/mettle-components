88d9de2344fd43d5238f93bc992de7b5989d5003
aanikiej <aanikiej@its.jnj.com>
2022-03-16 11:33:23 -0400

    renamed web component to sidebar

--- src/containers/mettle-sidebar.js
+++ src/containers/mettle-sidebar.js
@@ -1,4 +1,4 @@
-const TAG_NAME = 'navbar-adam'
+const TAG_NAME = 'mettle-sidebar'
 
 if (!window.customElements.get(TAG_NAME)) {
   window.customElements.define(TAG_NAME, class extends HTMLElement {
@@ -8,26 +8,31 @@ if (!window.customElements.get(TAG_NAME)) {
       this.attachShadow({ mode: 'open' })
         .appendChild(this._generateTemplate().content.cloneNode(true))
       this._allowedPositions = new Set(['left', 'right'])
-      this.$navbarNav = this.shadowRoot.querySelector('.navbar-nav')
-      this.$navbar = this.shadowRoot.querySelector('.navbar-open')
+      this.$sideBarData = this.shadowRoot.querySelector('.sidebar-data')
       this.isShowing = false
       this.position = 'left'
       this.width = this.hasAttribute('data-width') ? this.getAttribute('data-width') : '10rem'
-      this.$navbarNav.style.width = this.width
+      this.$sideBarData.style.width = this.width
       //see if there is a specified icon or if default one should be used
-      this.shadowRoot.querySelector('.navbar-icon').src = this.hasAttribute('icon') ? this.getAttribute('icon') : 'https://freesvg.org/img/menu-icon.png'
-      this.shadowRoot.querySelector('.navbar-icon-inside').src = this.hasAttribute('close-icon') ? this.getAttribute('close-icon') : 'https://freesvg.org/img/menu-icon.png'
+      this.shadowRoot.querySelector('.sidebar-expand-icon').src = this.hasAttribute('icon') ? this.getAttribute('icon') : 'https://freesvg.org/img/menu-icon.png'
+      this.shadowRoot.querySelector('.sidebar-close-icon').src = this.hasAttribute('close-icon') ? this.getAttribute('close-icon') : 'https://freesvg.org/img/menu-icon.png'
     }
 
     _generateTemplate() {
       const template = document.createElement('template')
       template.innerHTML = `
         <style>
-        .navbar-icon,
-        .navbar-icon-inside {
+        .sidebar-expand-icon,
+        .sidebar-close-icon {
           width: 2rem;
+          cursor: pointer;
         }
-        .navbar-nav {
+        .sidebar-close {
+          display: flex;
+          align-items: center;
+          justify-content: flex-end;
+        }
+        .sidebar-data {
           position: fixed;
           top: 0;
           height: 100%;
@@ -35,29 +40,20 @@ if (!window.customElements.get(TAG_NAME)) {
           overflow-x: hidden;
           transition: transform 150ms ease-in-out 25ms;
         }
-        .navbar-open {
-          cursor: pointer;
-          overflow-x: hidden;
-        }
-        .navbar-nav-close {
-          cursor: pointer;
-          padding-left: .5rem;
-          padding-top: .5rem;
-        }
         .nav-open {
           overflow: hidden;
           height: 100%;
         }
         </style>
-        <div class="navbar-container">
-          <div class="navbar-open">
-            <img class="navbar-icon">
+        <div class="sidebar-container">
+          <div class="sidebar-expand">
+            <img class="sidebar-expand-icon">
           </div>
-          <nav class="navbar-nav" part="nav">
-            <div class="navbar-nav-close">
-              <img class="navbar-icon-inside">
+          <nav class="sidebar-data" part="nav">
+            <div class="sidebar-close">
+              <img class="sidebar-close-icon">
             </div>
-            <slot name="navbar-content"> </slot>
+            <slot name="sidebar-content"> </slot>
           </nav>
         </div>
       `
@@ -65,16 +61,16 @@ if (!window.customElements.get(TAG_NAME)) {
     }
 
     connectedCallback() {
-      //determine navbar positioning
+      //determine sidebar positioning
       this._positionAt()
-      //hide navbar at start
+      //hide sidebar at start
       this._hide()
       //closeMenu
-      this.shadowRoot.querySelector('.navbar-open').addEventListener('click', () => {
+      this.shadowRoot.querySelector('.sidebar-expand').addEventListener('click', () => {
         this._toggleNav()
       })
       //openMenu
-      this.shadowRoot.querySelector('.navbar-nav-close').addEventListener('click', () => {
+      this.shadowRoot.querySelector('.sidebar-close-icon').addEventListener('click', () => {
         this._toggleNav()
       })
     }
@@ -94,31 +90,31 @@ if (!window.customElements.get(TAG_NAME)) {
     }
 
     _show() {
-      //translate the navbar to be visible on page
-      this.$navbarNav.style.transform = 'translate(0rem)'
-      //add style properties from document.body that prevent scrolling when navbar is open
+      //translate the sidebar to be visible on page
+      this.$sideBarData.style.transform = 'translate(0rem)'
+      //add style properties from document.body that prevent scrolling when sidebar is open
       document.body.style.overflow = 'hidden'
       document.body.style.height = '100%'
       this.isShowing = true
     }
 
     _hide() {
-      //determine which way the navbar should hide depending on navbar positioning
-      this.$navbarNav.style.transform = (this.position === 'left') ? `translate(-${this.width})` : `translate(${this.width})`
-      //remove style properties from document.body that prevent scrolling when navbar is open
+      //determine which way the sidebar should hide depending on sidebar positioning
+      this.$sideBarData.style.transform = (this.position === 'left') ? `translate(-${this.width})` : `translate(${this.width})`
+      //remove style properties from document.body that prevent scrolling when sidebar is open
       document.body.style.removeProperty('overflow')
       document.body.style.removeProperty('height')
       this.isShowing = false
     }
 
     _positionAt() {
-      //get position from attribute for navbar, if none detected, default to left
+      //get position from attribute for sidebar, if none detected, default to left
       const attrPosition = this.getAttribute('data-position')
       this.position = this._allowedPositions.has(attrPosition) ? attrPosition : 'left'
       if (this.position === 'left') {
-        this.$navbarNav.style.left = '0'
+        this.$sideBarData.style.left = '0'
       } else {
-        this.$navbarNav.style.right = '0'
+        this.$sideBarData.style.right = '0'
       }
     }
 
