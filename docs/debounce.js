6fb35b623a53bd42bd4e868501db74adec639060
aanikiej <aanikiej@its.jnj.com>
2022-04-05 15:43:14 -0400

    added storybook page for sidebar

--- docs/debounce.js
+++
@@ -1,8 +0,0 @@
-const Debounce = (func, wait = 50) => {
-  let timer
-  return (...params) => {
-    window.clearTimeout(timer)
-    timer = window.setTimeout(func.bind.apply(func, [null].concat(params)), wait)
-  }
-}
-export default Debounce
