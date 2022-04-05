13e94b2094fd4f22541e8bf2717b1ae7bc6b8e97
aanikiej <aanikiej@its.jnj.com>
2022-04-05 15:40:36 -0400

    started to add storybook for sidebar

--- test/unit/elements/mettle-sidebar.test.js
+++ test/unit/elements/mettle-sidebar.test.js
@@ -1,3 +1,5 @@
+import { wait } from "../helper"
+
 const ELEM_TAG_NAME = 'mettle-sidebar'
 
 describe(ELEM_TAG_NAME, () => {
@@ -6,12 +8,19 @@ describe(ELEM_TAG_NAME, () => {
   const elemTag = ELEM_TAG_NAME
 
   beforeEach(() => {
-    $el = globalThis.document.createElement(elemTag)
-    globalThis.document.body.appendChild($el)
+    globalThis.document.body.insertAdjacentHTML('afterbegin', '<div id="test">Testing</div>')
+    globalThis.document.body.insertAdjacentHTML('afterbegin', '<mettle-sidebar data-for="test"><div slot="content">Content Testing</div></mettle-sidebar>')
+    $el = globalThis.document.querySelector(elemTag)
   })
 
   afterEach(() => {
-    $el.remove()
+    const target = globalThis.document.getElementById('test')
+    if (target) {
+      target.remove()
+    }
+    if ($el) {
+      $el.remove()
+    }
     $el = null
   })
 
@@ -37,10 +46,49 @@ describe(ELEM_TAG_NAME, () => {
     // })
 
     it('should return boolean for isOpened', () => {
-      const opened = $el.isOpened()
+      const opened = $el.isOpened
       expect(opened).toBeFalse()
     })
-    
+
+    it('should be able to toggle the sidebar to open and close', () => {
+      $el.toggleNav()
+      expect($el.getAttribute('open')).toBeDefined()
+      $el.toggleNav()
+      expect($el.getAttribute('open')).toBeNull()
+    })
+
+    it('should be able to change sidebar to main navigation sidebar', () => {
+      const componentStyle = globalThis.getComputedStyle($el.$sidebar)
+      const firstPosition  = componentStyle.getPropertyValue('position')
+      $el.setAttribute('main-sidebar', '')
+      const secondPosition  = componentStyle.getPropertyValue('position')
+      expect(firstPosition).not.toEqual(secondPosition)
+    })
+
+    it('should be able to change position to right', () => {
+      const componentStyle = globalThis.getComputedStyle($el.$sidebar)
+      const firstPosition = componentStyle.getPropertyValue('right')
+      $el.setAttribute('data-position', 'right')
+      const secondPosition = componentStyle.getPropertyValue('right')
+      console.log(firstPosition, secondPosition)
+      expect(firstPosition).not.toEqual(secondPosition)
+    })
+
+    it('should be able to change position to right when used as a main sidebar', () => {
+      $el.remove()
+      globalThis.document.body.insertAdjacentHTML('afterbegin', '<mettle-sidebar data-for="test" main-sidebar><div slot="content">Content Testing</div></mettle-sidebar>')
+      const $secondEl = globalThis.document.querySelector(elemTag)
+      const componentStyle = globalThis.getComputedStyle($secondEl.$sidebar)
+      $secondEl.open()
+
+      const firstPosition = componentStyle.getPropertyValue('width')
+      const testPosition = componentStyle.getPropertyValue('left')
+      $el.setAttribute('data-position', 'right')
+      const secondPosition = componentStyle.getPropertyValue('left')
+
+      console.log(firstPosition, testPosition, secondPosition)
+    })
+
     it('should return an object of event type values', () => {
       expect($el.EVENT_TYPES).toEqual(jasmine.any(Object))
     })
